f79d149ba98cf7170a6dbe18b40b6eb8c18102db
jamjamzo <jaeyoun92@gmail.com>
2023-09-15 01:46:48 +0900

    230915 조회 및 작성

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+late SharedPreferences prefs;
+void main() async {
+  // main() 함수에서 async를 쓰려면 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // shared_preferences 인스턴스 생성
+  prefs = await SharedPreferences.getInstance();
+
   runApp(MyApp());
 }
 
@@ -11,12 +20,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // SharedPreferences에서 온보딩 완료 여부 조회
+    // isOnboarded에 해당하는 값에서 null을 반환하는 경우 false 할당
+    bool isOnboarded = prefs.getBool("isOnboarded") ?? false;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.getTextTheme('Jua'),
       ),
-      home: OnboardingPage(),
+      home: isOnboarded ? HomePage() : OnboardingPage(),
     );
   }
 }
@@ -71,6 +83,9 @@ class OnboardingPage extends StatelessWidget {
         next: Text("Next", style: TextStyle(fontWeight: FontWeight.w600)),
         done: Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
         onDone: () {
+          // Done 클릭시 isOnboarded = true로 저장
+          prefs.setBool("isOnboarded", true);
+
           // When done button is press
           Navigator.pushReplacement(
             context,
@@ -82,21 +97,143 @@ class OnboardingPage extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> bucketList = ['여행가기']; // 전체 버킷리스트 목록
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('버킷리스트'),
+        actions: [
+          // 삭제 버튼
+          IconButton(
+            onPressed: () {
+              // SharedPreferences에 저장된 모든 데이터 삭제
+              prefs.clear();
+            },
+            icon: Icon(Icons.delete),
+          )
+        ],
       ),
-      body: Center(
-        child: Text(
-          '환영합니다',
-          style: TextStyle(
-            fontSize: 24,
-          ),
+      body: bucketList.isEmpty
+          ? Center(child: Text("버킷 리스트를 작성해 주세요."))
+          : ListView.builder(
+              itemCount: bucketList.length, // bucketList 개수 만큼 보여주기
+              itemBuilder: (context, index) {
+                String bucket = bucketList[index]; // index에 해당하는 bucket 가져오기
+                return ListTile(
+                  // 버킷 리스트 할 일
+                  title: Text(
+                    bucket,
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                  // 삭제 아이콘 버튼
+                  trailing: IconButton(
+                    icon: Icon(CupertinoIcons.delete),
+                    onPressed: () {
+                      // 삭제 버튼 클릭시
+                      print('$bucket : 삭제하기');
+                    },
+                  ),
+                  onTap: () {
+                    // 아이템 클릭시
+                    print('$bucket : 클릭 됨');
+                  },
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          // + 버튼 클릭시 버킷 생성 페이지로 이동
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => CreatePage()),
+          );
+        },
+      ),
+    );
+  }
+}
+
+/// 버킷 생성 페이지
+class CreatePage extends StatefulWidget {
+  const CreatePage({Key? key}) : super(key: key);
+
+  @override
+  State<CreatePage> createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  // TextField의 값을 가져올 때 사용합니다.
+  TextEditingController textController = TextEditingController();
+  // 경고 메시지
+  String? error;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("버킷리스트 작성"),
+        // 뒤로가기 버튼
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.chevron_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            // 텍스트 입력창
+            TextField(
+              controller: textController,
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: "하고 싶은 일을 입력하세요",
+                errorText: error,
+              ),
+            ),
+            SizedBox(height: 32),
+            // 추가하기 버튼
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: ElevatedButton(
+                child: Text(
+                  "추가하기",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: () {
+                  // 추가하기 버튼 클릭시
+                  String job = textController.text;
+                  if (job.isEmpty) {
+                    setState(() {
+                      error = '내용을 입력해주세요';
+                    });
+                  } else {
+                    setState(() {
+                      error = null;
+                    });
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
