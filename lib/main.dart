c5e3d2750c8be086a01bd61895d650c749546438
jaeyoun92 <jaeyoun92@gmail.com>
2023-09-15 15:22:39 +0900

    230915 버킷리스트 앱 완성 & 팝업 띄우기 성공

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:bucketlist/bucket_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 late SharedPreferences prefs;
@@ -12,7 +14,14 @@ void main() async {
   // shared_preferences 인스턴스 생성
   prefs = await SharedPreferences.getInstance();
 
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => BucketService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -97,71 +106,123 @@ class OnboardingPage extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
+///  버킷 클래스
+class Bucket {
+  String job; // 할 일
+  bool isDone; // 완료 여부
+
+  Bucket(this.job, this.isDone); // 생성자
+}
+
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<String> bucketList = ['여행가기']; // 전체 버킷리스트 목록
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('버킷리스트'),
-        actions: [
-          // 삭제 버튼
-          IconButton(
-            onPressed: () {
-              // SharedPreferences에 저장된 모든 데이터 삭제
-              prefs.clear();
-            },
-            icon: Icon(Icons.delete),
-          )
-        ],
-      ),
-      body: bucketList.isEmpty
-          ? Center(child: Text("버킷 리스트를 작성해 주세요."))
-          : ListView.builder(
-              itemCount: bucketList.length, // bucketList 개수 만큼 보여주기
-              itemBuilder: (context, index) {
-                String bucket = bucketList[index]; // index에 해당하는 bucket 가져오기
-                return ListTile(
-                  // 버킷 리스트 할 일
-                  title: Text(
-                    bucket,
-                    style: TextStyle(
-                      fontSize: 24,
-                    ),
-                  ),
-                  // 삭제 아이콘 버튼
-                  trailing: IconButton(
-                    icon: Icon(CupertinoIcons.delete),
-                    onPressed: () {
-                      // 삭제 버튼 클릭시
-                      print('$bucket : 삭제하기');
-                    },
-                  ),
-                  onTap: () {
-                    // 아이템 클릭시
-                    print('$bucket : 클릭 됨');
+    return Consumer<BucketService>(
+      builder: (context, bucketService, child) {
+        List<Bucket> bucketList = bucketService.bucketList;
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('버킷리스트'),
+            actions: [
+              // 삭제 버튼
+              IconButton(
+                onPressed: () {
+                  // SharedPreferences에 저장된 모든 데이터 삭제
+                  prefs.clear();
+                },
+                icon: Icon(Icons.delete),
+              )
+            ],
+          ),
+          body: bucketList.isEmpty
+              ? Center(child: Text("버킷 리스트를 작성해 주세요."))
+              : ListView.builder(
+                  itemCount: bucketList.length, // bucketList 개수 만큼 보여주기
+                  itemBuilder: (context, index) {
+                    Bucket bucket =
+                        bucketList[index]; // index에 해당하는 bucket 가져오기
+                    return ListTile(
+                      // 버킷 리스트 할 일
+                      title: Text(
+                        bucket.job,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: bucket.isDone ? Colors.grey : Colors.black,
+                          decoration: bucket.isDone
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.none,
+                        ),
+                      ),
+                      // 삭제 아이콘 버튼
+                      trailing: IconButton(
+                        icon: Icon(CupertinoIcons.delete),
+                        onPressed: () {
+                          // 삭제 버튼 클릭시
+                          // bucketService.deleteBucket(index);
+                          showDeleteDialog(context, index);
+                        },
+                      ),
+                      onTap: () {
+                        // // 아이템 클릭시
+                        bucket.isDone = !bucket.isDone;
+                        bucketService.updateBucket(bucket, index);
+                      },
+                    );
                   },
-                );
+                ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () async {
+              // + 버튼 클릭시 버킷 생성 페이지로 이동
+              String? job = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => CreatePage()),
+              );
+              if (job != null) {
+                // setState(() {
+                //   Bucket newBucket = Bucket(job, false);
+                //   bucketList.add(newBucket);
+                // });
+              }
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  void showDeleteDialog(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('정말로 삭제하시겠습니까?'),
+          actions: [
+            // 취소 버튼
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
               },
+              child: Text("취소"),
             ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          // + 버튼 클릭시 버킷 생성 페이지로 이동
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (_) => CreatePage()),
-          );
-        },
-      ),
+            // 확인 버튼
+            TextButton(
+              onPressed: () {
+                // BucketService의 deleteBucket 함수를 호출하여 데이터 삭제
+                Provider.of<BucketService>(context, listen: false)
+                    .deleteBucket(index);
+                Navigator.pop(context);
+              },
+              child: Text(
+                "확인",
+                style: TextStyle(color: Colors.pink),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -229,6 +290,10 @@ class _CreatePageState extends State<CreatePage> {
                     setState(() {
                       error = null;
                     });
+                    //BucketService 가져오기
+                    BucketService bucketService = context.read<BucketService>();
+                    bucketService.createBucket(job);
+                    Navigator.pop(context, job);
                   }
                 },
               ),
